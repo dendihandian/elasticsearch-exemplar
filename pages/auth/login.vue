1a90e3fa52e591a420eabd2073569b075ea3a4ad
Dendi Handian <handian.dendi@gmail.com>
2018-08-16 20:24:39 +0700

    add placeholder pages

---
+++ pages/auth/login.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+  <v-container>Login</v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
---
+++ pages/products/index.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+  <v-container>Products</v-container>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css">
+</style>
