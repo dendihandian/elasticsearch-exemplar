f4198d0ef441a6cc3b15dc168375dd7d6b135ead
Dendi Handian <handian.dendi@gmail.com>
2018-08-17 00:44:54 +0700

    add login case

--- pages/auth/login.vue
+++ pages/auth/login.vue
@@ -1,10 +1,68 @@
 <template lang="html">
-  <v-container>Login</v-container>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm8 offset-sm2>
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <form @submit.prevent="submit">
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="email"
+                      label="Email"
+                      id="email"
+                      v-model="email"
+                      type="email"
+                      required
+                    ></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="password"
+                      label="Password"
+                      id="password"
+                      v-model="password"
+                      type="password"
+                      required
+                    ></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row class>
+                  <v-flex class="text-xs-center">
+                    <v-btn class="warning black--text" type="submit">Sign In</v-btn>
+                  </v-flex>
+                </v-layout>
+              </form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
-export default {
-}
+  export default {
+    data () {
+      return {
+        email: null,
+        password: null
+      }
+    },
+    methods: {
+      submit () {
+        this.$store.dispatch('authentication', {
+          email: this.email,
+          password: this.password
+        }).then(() => {
+          this.$router.push('/')
+        })
+      }
+    }
+  }
 </script>
 
 <style lang="css">
--- store/index.js
+++ store/index.js
@@ -3,21 +3,53 @@ import Vuex from 'vuex'
 const createStore = () => {
   return new Vuex.Store({
     state: {
-      drawer : false
+      drawer: false,
+      token: null,
+      user: null
     },
     mutations: {
       drawerToggle (state) {
         state.drawer = !state.drawer
+      },
+      setToken (state, token) {
+        state.token = token
+      },
+      setUser (state, authenticatedUser) {
+        state.user = authenticatedUser
+      },
+      clearToken (state) {
+        state.token = null
       }
     },
     actions: {
       drawerToggle (vuexContext) {
         vuexContext.commit('drawerToggle')
+      },
+      authentication (vuexContext, authData) {
+        return this.$axios.$post('/auth/login', {
+          email: authData.email,
+          password: authData.password
+        })
+        .then(result => {
+          // state
+          vuexContext.commit('setToken', result.data.token)
+          vuexContext.commit('setUser', result.data.user)
+          // localStorage
+          localStorage.setItem('token', result.data.token)
+          localStorage.setItem("tokenExpiration", Number.parseInt(result.data.tokenExpiration) * 1000)
+        })
+        .catch(e => console.log(e))
       }
     },
     getters: {
       drawer (state) {
         return state.drawer
+      },
+      isAuthenticated (state) {
+        return state.token != null
+      },
+      authenticatedUser (state) {
+        return state.user
       }
     }
   })
