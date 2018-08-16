66ee2d1261a6bb8e07cdd14bad193692c8a39f3b
Dendi Handian <handian.dendi@gmail.com>
2018-08-16 19:56:48 +0700

    add toolbar, sidebar, footer components

---
+++ components/AppFooter.vue
@@ -0,0 +1,18 @@
+<template lang="html">
+  <v-footer :fixed="fixed" app>
+    <span>&copy; 2017</span>
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        fixed: true
+      }
+    }
+  }
+</script>
+
+<style lang="css">
+</style>
---
+++ components/AppToolbar.vue
@@ -0,0 +1,32 @@
+<template lang="html">
+  <v-toolbar fixed app :clipped-left="clipped">
+    <v-toolbar-side-icon @click="drawerToggle"></v-toolbar-side-icon>
+    <v-toolbar-title v-text="title"></v-toolbar-title>
+    <v-spacer></v-spacer>
+  </v-toolbar>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        clipped: false,
+        fixed: false,
+        title: 'Vuetify.js'
+      }
+    },
+    computed: {
+      drawer () {
+        return this.$store.getters.drawer
+      }
+    },
+    methods: {
+      drawerToggle () {
+        this.$store.dispatch('drawerToggle')
+      }
+    }
+  }
+</script>
+
+<style lang="css">
+</style>
