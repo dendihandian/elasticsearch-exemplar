c579202513b294ed280637898e98b0cdfc93cf8b
Dendi Handian <handian.dendi@gmail.com>
2018-08-19 15:13:41 +0700

    remove any auth and the state (preparing to use @nuxtjs/auth module)

--- middleware/auth.js
+++
@@ -1,9 +0,0 @@
-export default function (context) {
-  // console.log('[middleware] auth')
-  if (!context.store.getters.isAuthenticated) {
-    context.store.dispatch('setSnackbar', {
-      message: 'You need to login first to view this page'
-    })
-    context.redirect('/')
-  }
-}
--- store/index.js
+++ store/index.js
@@ -4,30 +4,16 @@ const createStore = () => {
   return new Vuex.Store({
     state: {
       drawer: false,
-      token: null,
-      user: null,
       snackbar: null
     },
     mutations: {
       drawerToggle (state) {
         state.drawer = !state.drawer
       },
-      setToken (state, token) {
-        state.token = token
-      },
-      setUser (state, authenticatedUser) {
-        state.user = authenticatedUser
-      },
       setSnackbar (state, snackbarData) {
         state.snackbar = snackbarData
       },
-      clearToken (state) {
-        state.token = null
-      },
-      clearUser (state) {
-        state.user = null
-      },
-      clearAlert (state) {
+      clearSnackbar (state) {
         state.snackbar = null
       }
     },
@@ -37,42 +23,11 @@ const createStore = () => {
       },
       setSnackbar (vuexContext, snackbarData) {
         vuexContext.commit('setSnackbar', snackbarData)
-      },
-      authentication (vuexContext, authData) {
-        return this.$axios.$post('/auth/login', {
-          email: authData.email,
-          password: authData.password
-        })
-        .then(result => {
-          // state
-          vuexContext.commit('setToken', result.data.token)
-          vuexContext.commit('setUser', result.data.user)
-          // localStorage
-          localStorage.setItem('token', result.data.token)
-          localStorage.setItem("tokenExpiration", Number.parseInt(result.data.tokenExpiration) * 1000)
-        })
-        .catch(e => console.log(e))
-      },
-      logout (vuexContext) {
-        // state
-        vuexContext.commit('clearToken')
-        vuexContext.commit('clearUser')
-        // localStorage
-        if (process.client) {
-          localStorage.removeItem('token')
-          localStorage.removeItem('tokenExpiration')
-        }
       }
     },
     getters: {
       drawer (state) {
         return state.drawer
-      },
-      isAuthenticated (state) {
-        return state.token != null
-      },
-      authenticatedUser (state) {
-        return state.user
       },
       snackbar (state) {
         return state.snackbar
