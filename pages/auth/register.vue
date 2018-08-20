4371193117300e33f7f34187c7053bc77610d5c5
Dendi Handian <handian.dendi@gmail.com>
2018-08-20 15:28:06 +0700

    finish register page

--- pages/auth/register.vue
+++ pages/auth/register.vue
@@ -1,9 +1,92 @@
 <template lang="html">
-  <v-container>Register</v-container>
+  <v-container>
+    <v-layout row>
+      <v-flex xs12 sm8 offset-sm2>
+        <v-card>
+          <v-card-text>
+            <v-container>
+              <form @submit.prevent="submit">
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="username"
+                      label="Username"
+                      id="username"
+                      v-model="username"
+                      type="text"
+                      required
+                    ></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="email"
+                      label="Email"
+                      id="email"
+                      v-model="email"
+                      type="email"
+                      required
+                    ></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row>
+                  <v-flex xs12>
+                    <v-text-field
+                      name="password"
+                      label="Password"
+                      id="password"
+                      v-model="password"
+                      type="password"
+                      required
+                    ></v-text-field>
+                  </v-flex>
+                </v-layout>
+                <v-layout row class>
+                  <v-flex class="text-xs-center">
+                    <v-btn class="warning black--text" type="submit">Register</v-btn>
+                  </v-flex>
+                </v-layout>
+              </form>
+            </v-container>
+          </v-card-text>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      username: '',
+      email: '',
+      password: '',
+    }
+  },
+  methods: {
+    async submit() {
+      try {
+        await this.$axios.post('/auth/register', {
+          username: this.username,
+          email: this.email,
+          password: this.password
+        })
+
+        await this.$auth.loginWith('local', {
+          data: {
+            email: this.email,
+            password: this.password
+          },
+        })
+
+        this.$router.push('/')
+      } catch (e) {
+        this.error = e.response.data.message
+      }
+    }
+  }
 }
 </script>
 
