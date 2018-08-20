312dcfc53d5b5e53bc728d1058402d2e6d8e4be2
Dendi Handian <handian.dendi@gmail.com>
2018-08-20 23:30:18 +0700

    add link to product of products

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -8,24 +8,26 @@
       Products are set publicly, which means anyone can read and write without authentication.
     </v-alert>
     <v-layout row wrap mt-4>
-      <v-flex
-        v-for="product in products"
-        :key="product.id"
-        xs12 sm6 md4 lg3
-        >
-        <v-card flat tile pa-2>
-          <v-card-media
-            :src="`https://unsplash.it/150/300?image=${Math.floor(Math.random() * 100) + 1}`"
-            height="150px"
-          ></v-card-media>
-          <v-card-title primary-title>
-            <div>
-              <div class="headline">{{ product.name.substring(0,16) }}</div>
-              <span class="orange--text">${{ product.price }}</span>
-            </div>
-          </v-card-title>
-        </v-card>
-      </v-flex>
+        <v-flex
+          v-for="product in products"
+          :key="product.id"
+          xs12 sm6 md4 lg3
+          >
+          <router-link :to="'/products/' + product.id">
+            <v-card flat tile pa-2>
+              <v-card-media
+                :src="`https://unsplash.it/150/300?image=${Math.floor(Math.random() * 100) + 1}`"
+                height="150px"
+              ></v-card-media>
+              <v-card-title primary-title>
+                <div>
+                  <div class="headline">{{ product.name.substring(0,16) }}</div>
+                  <span class="orange--text">${{ product.price }}</span>
+                </div>
+              </v-card-title>
+            </v-card>
+          </router-link>
+        </v-flex>
     </v-layout>
   </v-container>
 </template>
@@ -43,26 +45,16 @@ export default {
     }
   },
   created () {
-    // console.log('created')
-
     this.$axios.$get('/products')
       .then(result => {
-        // console.log(result)
         this.$store.dispatch('setProducts', result.data)
       })
       .catch(error => {
-        // console.log(error)
         this.$store.dispatch('setSnack', {
           message: error.message,
           color: 'red'
         })
       })
-  },
-  mounted () {
-    console.log('mounted')
-  },
-  destroyed () {
-    console.log('destroyed')
   }
 }
 </script>
