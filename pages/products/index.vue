589f4c2a6a7fd531c413722633ecde5e18ac4de3
Dendi Handian <handian.dendi@gmail.com>
2018-08-24 20:51:30 +0700

    fix indentation

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -8,26 +8,26 @@
       Products are set publicly, which means anyone can read and write without authentication.
     </v-alert>
     <v-layout row wrap mt-4>
-        <v-flex
-          v-for="product in products"
-          :key="product.id"
-          xs12 sm6 md4 lg3
-          >
-          <router-link :to="'/products/' + product.id">
-            <v-card flat tile style="padding:10px;">
-              <v-card-media
-                :src="`https://unsplash.it/150/300?image=${Math.floor(Math.random() * 100) + 1}`"
-                height="150px"
-              ></v-card-media>
-              <v-card-title primary-title>
-                <div>
-                  <div class="headline">{{ product.name.substring(0,16) }}</div>
-                  <span class="orange--text">${{ product.price }}</span>
-                </div>
-              </v-card-title>
-            </v-card>
-          </router-link>
-        </v-flex>
+      <v-flex
+        v-for="product in products"
+        :key="product.id"
+        xs12 sm6 md4 lg3
+        >
+        <router-link :to="'/products/' + product.id">
+          <v-card flat tile style="padding:10px;">
+            <v-card-media
+              :src="`https://unsplash.it/150/300?image=${Math.floor(Math.random() * 100) + 1}`"
+              height="150px"
+            ></v-card-media>
+            <v-card-title primary-title>
+              <div>
+                <div class="headline">{{ product.name.substring(0,16) }}</div>
+                <span class="orange--text">${{ product.price }}</span>
+              </div>
+            </v-card-title>
+          </v-card>
+        </router-link>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
